852b8a794b084ee316b1eebfe6fb29ec4cdae583
MFajriasyah46 <mfajriansyah23@mhs.politala.ac.id>
2024-09-16 15:04:54 -1200

    Perbaikan tampilan

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('app');
+    return view('home');
+});
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/testing', function () {
+    return view('testing');
 });
