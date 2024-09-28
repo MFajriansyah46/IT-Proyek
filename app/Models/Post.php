052308cb1fa0cb0d05f6f524303d4c5ea36b6b7b
MFajriasyah46 <mfajriansyah23@mhs.politala.ac.id>
2024-09-27 23:56:32 -1200

    relasi tabel

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model {
     use HasFactory;
     protected $fillabel = ['title','author','slug','body'];
     protected $casts = ['created_at' => 'datetime'];
+
+    public function author(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,49 +1,43 @@
 <?php
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use  App\Models\Post;
+use  App\Models\User;
+use  App\Models\Category;
 
 Route::get('/', function () {
     return view('home',['title' => 'Home Page']);
 });
 
 Route::get('/posts', function () {
-    return view('posts',['title' => 'Blog','posts' => Post::all()]);
+    return view('posts',[
+        'title' => 'Blog',
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('/posts/{post:slug}', function(Post $post) {
 
-    return view('post',['title' => 'Single Post','post' => $post]);
+    return view('post',[
+        'title' => 'Single Post',
+        'post' => $post
+    ]);
 });
 
+Route::get('/authors/{user:username}', function(User $user) {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Route::get('/login', function () {
-    return view('login');
+    return view('posts',[
+        'title' => count($user->posts) . ' Articles by '.$user->name,
+        'posts' => $user->posts
+    ]);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
+Route::get('/category/{category:slug}', function(Category $category) {
 
-
-Route::get('/welcome', function () {
-    return view('welcome');
+    return view('posts',[
+        'title' => $category->name,
+        'posts' => $category->posts
+    ]);
 });
