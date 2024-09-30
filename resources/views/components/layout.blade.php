a2ee87ca7824031242b8f3f21efb3e3ec08192e8
MFajriasyah46 <mfajriansyah23@mhs.politala.ac.id>
2024-09-29 14:40:36 -1200

    pasang jquery

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,25 +1,22 @@
 <!DOCTYPE html>
 <html lang="en" class="h-full bg-gray-100">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        @vite('resources/css/app.css')
-        @vite('resources/js/jquery-3.7.1.min.js')
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-        <title>Bang Raja</title>
-    </head class="h-full">
-    <body class="flex h-screen w-screen fixed">
-        <x-navbar></x-navbar>
-        <main class="flex-1">
-            <header class="shadow-lg relative">
-                <div class="px-4 py-8 bg-yellow-300 shadow m-0">
-                </div>
-            </header>
-            <div class="container mx-auto px-4 h-screen overflow-scroll">
-                <h1 class="text-4xl font-bold text-gray-800 mt-8 mb-6"> {{ $title }} </h1>
-                {{ $slot }}
-            </div>
-        </main>
-    </body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <script src="/assets/jquery-3.7.1.js"></script>
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <title>Bang Raja</title>
+  </head class="h-full">
+  <body class="flex h-screen w-screen fixed">
+    <x-navbar></x-navbar>
+    <main class="flex-1">
+      <header class="shadow-lg relative">
+        <div class="px-4 py-8 bg-gray-50 shadow m-0"></div>
+      </header>
+      <h1 class="text-4xl font-bold text-gray-800 mt-8 mb-6 mx-auto px-4 relative"> {{ $title }} </h1>
+      <div class="container mx-auto px-4 h-screen overflow-scroll">{{ $slot }}</div>
+    </main>
+  </body>
 </html>
--- resources/views/components/table/header.blade.php
+++ resources/views/components/table/header.blade.php
@@ -1,10 +1,10 @@
 <table class="min-w-full">
     <thead>
-        <tr class="border-b-2"> 
+        <tr class="border-b-2 "> 
             @foreach ($headers as $header)
-                <th class="px-2 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"> {{ $header }} </th>
+                <th class="px-2 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider sticky top-0"> {{ $header }} </th>
             @endforeach
         </tr>
     </thead>
-    <tbody class="bg-white divide-y divide-gray-200"> {{ $slot }} </tbody>
+    <tbody class="bg-white divide-y divide-gray-200 "> {{ $slot }} </tbody>
 </table>
