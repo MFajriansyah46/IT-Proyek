93127c66f8e08d7c458a73e34645cc7be8f72ec4
Zainal <zainal23@mhs.politala.ac.id>
2024-10-09 09:12:55 +0800

    tambah informasi web

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,11 @@
   <x-slot:title>{{ $title }}</x-slot>
   <body>
     <h1>WELCOME MY HOME PAGE</h1>
+    <article>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Rerum doloribus ullam omnis exercitationem quaerat incidunt laboriosam quos iste.
+         Quae est natus quasi consequatur porro incidunt alias perspiciatis ratione facilis illo.</p>
+    </article>
+    
   </body>
 </x-Layout>
